260e957d6dddeb460ab854cdf15c91282afaeac7
Architector WASPACE <architector.waspace@gmail.com>
2019-07-31 16:00:51 +0300

    git add properties and methods to HttpRequest

--- src/http_request/http_request.ts
+++ src/http_request/http_request.ts
@@ -31,6 +31,23 @@ export class HttpRequest {
     });
   }
 
+  get_response_header(
+    key: string
+  ): string {
+    return this.xhr.getResponseHeader(key);
+  }
+
+  get_response_bytes(): number[] {
+    return this.xhr.getResponseBytes();
+  }
+
+  set_request_header(
+    key: string,
+    value: string
+  ) {
+    this.xhr.setRequestHeader(key, value);
+  }
+
   get status(): number {
     return this.xhr.status;
   }
